66b8f7af027c3c11a4ce5285984884aac8c7254c
PBYAB <106178832+PBYAB@users.noreply.github.com>
2023-05-14 20:14:01 +0200

    No cos tu dziala

--- src/main/java/com/example/projekt/StoreEverything.java
+++ src/main/java/com/example/projekt/StoreEverything.java
@@ -4,10 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ProjektApplication {
+public class StoreEverything {
 
     public static void main(String[] args) {
-        SpringApplication.run(ProjektApplication.class, args);
+        SpringApplication.run(StoreEverything.class, args);
     }
-
 }
---
+++ src/main/java/com/example/projekt/data/Category.java
@@ -0,0 +1,12 @@
+package com.example.projekt.data;
+
+import lombok.*;
+
+@Data
+@RequiredArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class Category {
+    private String name;
+}
